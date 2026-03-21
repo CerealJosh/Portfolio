6391cf47d0407e460f62f4c749e3ab22aca16e3f
Onu Joshua <onujoshua2001@gmail.com>
2026-03-21 11:20:28 +0100

    chore: Update SvelteKit production base path to `/Project_1`.

--- svelte.config.js
+++ svelte.config.js
@@ -18,7 +18,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/Portfolio' : '',
+			base: process.env.NODE_ENV === 'production' ? '/Project_1' : '',
 		}
 	}
 };
